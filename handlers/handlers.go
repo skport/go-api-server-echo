dacc0d0f3af4eeced8eb38ddd24c8ffe949ef722
skport <45557654+skport@users.noreply.github.com>
2023-01-10 19:25:27 +0900

    Install echo-swagger and add /swagger routing.

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -4,6 +4,8 @@ import (
 	"github.com/labstack/echo/v4"
 	//"github.com/labstack/echo/v4/middleware"
 
+	"github.com/swaggo/echo-swagger"
+
 	"skport/go-api-server-echo/services"
 )
 
@@ -15,8 +17,26 @@ func NewHandler() *Handlers {
 	return h
 }
 
+// @title           Swagger Example API
+// @version         1.0
+// @description     This is a sample server celler server.
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   API Support
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host      localhost:3000
+// @BasePath  /
+
+// @securityDefinitions.basic  BasicAuth
 func (h *Handlers) Init(e *echo.Echo, s *services.Services) {
 	e.GET("/healthcheck", h.HealthCheck)
+
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
 
 	e.GET("/", s.Hello)
 	e.GET("/albums", s.GetAlbums)
